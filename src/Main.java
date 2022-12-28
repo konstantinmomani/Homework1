362e04b5eaf1afface7e2f8823b41813e964ae07
konstantin <mom.konstantin@gmail.com>
2022-12-28 14:48:03 +0300

    Добавил методы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 () {
         System.out.println("Привет, Мир");
+    }
+    public static void task2 () {
+        System.out.println("Hellow, World");
     }
 }
